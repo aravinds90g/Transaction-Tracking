858227e1c1fbc05024a74d7370a729e424b45323
Aravind S <aravinds90g@gmail.com>
2025-02-22 20:48:18 +0530

    next js

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from "next";
 
+module.exports = {
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,
+  },
+};
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
